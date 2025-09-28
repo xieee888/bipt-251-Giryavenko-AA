50ff968597170d4f42b08cd8cc5530893a494fe1
xieee888 <giryavenko.aa@gmail.com>
2025-09-29 00:57:59 +0300

    Update lab4_dz.c

--- lab4_dz.c
+++ lab4_dz.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
+#include <locale.h>
 int main(){
+setlocale(LC_ALL, "Russian");
 int A, B;
-if ((A % 2 == 0 && B % 2 != 0) || (A % 2 != 0 && B % 2 == 0)) {
-    // Резать на 4 части
-    printf("Резать пиццу на 4 куска\n");
-} else {
-    // Резать на 6 частей
-    printf("Резать пиццу на 6 кусков\n");
+int pizza;
+int x;
+float peace;
+printf("Введите Ваня=>A или Петя=>B.\"уровень голода\" - четное число : \n");
+scanf("%i", &A);
+scanf("%i", &B);
+printf("Введите размер пиццы см^2: \n");
+scanf("%i", &pizza);
+if ((A % 2 == 0 && B % 2 != 0)) {
+    x = 4;
+    peace = (float)pizza / x;
+    printf("ВАНЯ ПРОГОЛОДАЛСЯ\n");
+    printf("4 куска по: %0.f см^2\n", peace);
+} 
+if (A % 2 != 0 && B % 2 == 0){
+    x = 4;
+    peace = (float)pizza / x;
+    printf("ПЕТЯ ПРОГОЛОДАЛСЯ\n");
+    printf("4 куска по: %0.f см^2\n", peace);
+}
+if (A % 2 != 0 && B % 2 != 0){
+    x = 6;
+    peace = (float)pizza / x;
+    printf("Ваня&Петя оба голодные либо оба сытые\n");
+    printf("6 кусков по: %0.f см^2\n", peace);
+}
+if (A % 2 == 0 && B % 2 == 0){
+    x = 6;
+    peace = (float)pizza / x;
+    printf("Ваня&Петя оба голодные либо оба сытые\n");
+    printf("6 кусков по: %0.f см^2\n", peace);
 }
 }
